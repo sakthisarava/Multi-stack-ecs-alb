aba8238acf17af35ff313341d1315cd1c83c8b87
sakthisarava <velsakthis5301@gmail.com>
2022-06-13 08:56:13 +0530

    Initial commit

---
+++ test/multi-stack-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MultiStackTest from '../lib/multi-stack-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/multi-stack-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MultiStackTest.MultiStackTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
